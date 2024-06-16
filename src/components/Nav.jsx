6650a0bc521409b70405cbba787fe4284a26ebcd
AlexisPiramide <220240@fppiramide.com>
2024-06-16 12:30:52 +0200

    s

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,15 +1,39 @@
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useWindowSize } from 'react-use';
+import '../styles/Nav.css';
+import logo from '../../public/user.png';
 
 export default function Nav() {
+  const { width, height } = useWindowSize();
+  const [windowstate, setWindowstate] = useState(false);
+
+
+  useEffect(() => {
+    if (width < 960) {
+      setWindowstate(true);
+    } else {
+      setWindowstate(false);
+    }
+  }, [width]);
   return (
     <nav>
-      <h1>Mi nav</h1>
-      <div>
-        <Link to="/">Inicio</Link>
-        <Link to="/nueva">Nueva cosa</Link>
-        <Link to="/login">Login</Link>
-        <Link to="/registro">Registro</Link>
-      </div>
+      {windowstate ?
+        <div className="zona-usuario-reducizo">
+          <button className="btn-dropdown">
+            <img src={logo} alt="logo"/>
+          </button>
+          <div className="dropdown">
+            <Link href="/registro">Crear una cuenta</Link>
+            <Link href="/login">Acceder</Link>
+          </div>
+        </div>
+        :
+        <div className="zona-usuario">
+          <Link href="/registro">Crear una cuenta</Link>
+          <Link href="/login">Acceder</Link>
+        </div>
+      }
     </nav>
   );
 }

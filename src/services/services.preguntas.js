653a9bd1fa03d87521cbf1d370f434115257ccb2
AlexisPiramide <220240@fppiramide.com>
2025-05-27 14:58:50 +0200

    .

--- src/services/services.preguntas.js
+++
@@ -1,9 +0,0 @@
-import URL from "./constante.js";
-
-const getPregunta = async () => {
-    const data = await fetch(URL+"/api/pregunta")
-    const json = await data.json();
-    return json;
-};
-  
-export { getPregunta };
